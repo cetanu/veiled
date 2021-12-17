09061e0c899cd00de54123b728a89f804522f317
Vasili Syrakis <vsyrakis@atlassian.com>
2021-12-17 15:25:56 +1100

    0.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+0.2.0
+-----
+Added encrypt-yaml and decrypt-yaml for recursively encrypting strings in yaml files
+
+0.1.0
+-----
+created utility
+
+includes ability to generate a private key, and encrypt/decrypt strings and files
--- veiled/cli.py
+++ veiled/cli.py
@@ -1,53 +1,84 @@
+import yaml
 import typer
+from typing import Dict, Any, MutableMapping
 from pathlib import Path
 from cryptography.fernet import Fernet
 
 app = typer.Typer()
-colors = typer.colors
-
-
-def green(msg):
-    return typer.style(msg, fg=colors.GREEN)
-
-
-def red(msg):
-    return typer.style(msg, fg=colors.RED)
 
 
 @app.command()
-def encrypt(secret: str, key: str):
+def encrypt(secret: str, key: str) -> None:
     suite = Fernet(key)
     typer.echo(suite.encrypt(secret.encode()))
 
 
 @app.command()
-def encrypt_file(path: Path, key: str, output: Path):
+def encrypt_file(path: Path, key: str, output: Path) -> None:
     suite = Fernet(key)
     content = suite.encrypt(path.read_bytes())
-    typer.echo(green("Read plaintext from {path}"))
+    typer.echo(f"Read plaintext from {path}")
     output.write_bytes(content)
-    typer.echo(green("Wrote secret to {output}"))
+    typer.echo(f"Wrote secret to {output}")
 
 
 @app.command()
-def decrypt(secret: str, key: str):
+def decrypt(secret: str, key: str) -> None:
     suite = Fernet(key)
     typer.echo(suite.decrypt(secret.encode()))
 
 
 @app.command()
-def decrypt_file(path: Path, key: str, output: Path):
+def decrypt_file(path: Path, key: str, output: Path) -> None:
     suite = Fernet(key)
     content = suite.decrypt(path.read_bytes())
-    typer.echo(green("Read secret from {path}"))
+    typer.echo(f"Read secret from {path}")
     output.write_bytes(content)
-    typer.echo(green("Wrote plaintext to {output}"))
+    typer.echo(f"Wrote plaintext to {output}")
+
+
+def walk_and_encrypt(structure: MutableMapping, crypto) -> None:
+    # The YAML is expected to contain a key:value structure
+    for k, v in structure.items():
+        # Ignore non-strings
+        if isinstance(v, str):
+            v = v.encode()
+            structure[k] = crypto(v).decode()
+        if isinstance(v, MutableMapping):
+            walk_and_encrypt(v, crypto)
+
+
+def veil_yaml(mode: str, path: Path, key: str, output: Path) -> None:
+    src, dst = "plaintext", "secret"
+    if mode == "encrypt":
+        src, dst == dst, src
+
+    suite = Fernet(key)
+    method = getattr(suite, mode)
+
+    y = yaml.safe_load(path.read_text())
+    typer.echo(f"Read secret from {path}")
+    walk_and_encrypt(y, method)
+    content = yaml.safe_dump(y)
+
+    output.write_text(content)
+    typer.echo(f"Wrote plaintext to {output}")
 
 
 @app.command()
-def generate_key():
+def decrypt_yaml(path: Path, key: str, output: Path) -> None:
+    veil_yaml("decrypt", path, key, output)
+
+
+@app.command()
+def encrypt_yaml(path: Path, key: str, output: Path) -> None:
+    veil_yaml("encrypt", path, key, output)
+
+
+@app.command()
+def generate_key() -> None:
     typer.echo(Fernet.generate_key().decode())
 
 
-def run():
+def run() -> None:
     app()
